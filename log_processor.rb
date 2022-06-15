e4b06f0affda53b4c4d0362af1d83bdb1e332b06
Andy <andynu@gmail.com>
2022-06-15 16:41:33 -0400

    Step processor in the direction of using LogEvent objects.
    This is a half step. It is still generating the log_statistics object in
    the same shape { path => [ip, ip, ...], ... }

--- log_processor.rb
+++ log_processor.rb
@@ -2,22 +2,20 @@
 # frozen_string_literal: true
 
 class LogProcessor
-  attr_accessor :log_file, :log_statistics
   attr_reader :page_views, :unique_page_views
+
+  LogEvent = Struct.new(:path, :ip)
 
   def initialize(file_name)
     @file_name = file_name
   end
 
   def process
-    @log_file = File.read(@file_name)
-    @log_statistics = Hash.new { |h, k| h[k] = [] } # auto vivifying array values.
-    log_file.each_line do |line|
-      path, ip = line.split(' ')
-      log_statistics[path] << ip
-    end
+    log_events = File.readlines(@file_name).map { |line| LogEvent.new(*line.split) }
+    log_statistics = log_events.group_by(&:path).transform_values{ |events| events.map(&:ip) }
+
     @page_views = hash_sort_decending log_statistics.transform_values(&:count)
-    @unique_page_views = hash_sort_decending log_statistics.transform_values {|ips| ips.uniq.count }
+    @unique_page_views = hash_sort_decending log_statistics.transform_values { |ips| ips.uniq.count }
     self
   end
 
