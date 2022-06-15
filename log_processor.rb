22e62d9c0a715e5d749c3678ed1fcf3506e68eac
Andy <andynu@gmail.com>
2022-06-15 16:05:06 -0400

    Switch from each_with_object to transform_values
    Ooh! The keys of the hash are the same before and after, we can further
    simplify with transform_values.

--- log_processor.rb
+++ log_processor.rb
@@ -20,12 +20,12 @@ class LogProcessor
   end
 
   def page_views
-    result = log_statistics.each_with_object({}) { |(path, ips), hash| hash[path] = ips.count }
+    result = log_statistics.transform_values(&:count)
     result.sort_by { |_path, count| count }.reverse
   end
 
   def unique_page_views
-    result = log_statistics.each_with_object({}) { |(path, ips), hash| hash[path] = ips.uniq.count }
+    result = log_statistics.transform_values {|ips| ips.uniq.count }
     result.sort_by { |_path, count| count }.reverse
   end
 end
